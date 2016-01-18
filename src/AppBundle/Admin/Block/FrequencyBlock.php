441baf6fdbc35ebe560897b1d563d28a9c821d66
Anton Serra <aserratorta@gmail.com>
2016-01-18 17:13:08 +0100

    create FrequencyBlock class

---
+++ src/AppBundle/Admin/Block/FrequencyBlock.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Admin\Block;
+
+use Doctrine\ORM\EntityManager;
+use Sonata\BlockBundle\Block\BaseBlockService;
+use Sonata\BlockBundle\Block\BlockContextInterface;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Class FrequencyBlock
+ *
+ * @category Block
+ * @package  AppBundle\Admin\Block
+ * @author   Anton Serra <aserratorta@gmail.com>
+ */
+class FrequencyBlock extends BaseBlockService
+{
+
+}
